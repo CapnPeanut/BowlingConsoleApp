de00f03a2ba53133cf6de7e5a4128a8f29a82d40
Captain Peanut <capnpeanut@gmail.com>
2017-03-08 22:07:27 -0500

    Start of some framework for the bowling problem.  Working on logic issues for spares and strikes.

--- Bowling Problem/Program.cs
+++ Bowling Problem/Program.cs
@@ -10,6 +10,69 @@ namespace Bowling_Problem
     {
         static void Main(string[] args)
         {
+            
+    
+
         }
+
+        public void Bowling()
+        {
+            // What variables will be needed
+            var rollsCount = 1;
+            var score = 0;
+            var frameScore = 0;
+            var NextRollOne = 0;
+            var NextRollTwo = 0;
+
+            for (int frames = 0; frames < 9; frames++)
+            {
+
+
+                if (frames == 9 && rollsCount <= 2)
+                {
+                    Console.WriteLine("Enter the number of pins knocked down on roll " + rollsCount);
+                    int rollOne = Convert.ToInt32(Console.ReadLine());
+                    if (rollOne == 10)
+                    {
+                        frameScore = 10;
+                        
+                        score += frameScore + NextRollOne + NextRollTwo;
+                        Console.WriteLine("Your score this frame was " + frameScore + " and your total score is " + score);
+                        rollsCount = 3;
+
+                    }
+                    else
+                    {
+
+                        rollsCount += 1;
+                        Console.WriteLine("Enter the number of pins knocked down on roll " + rollsCount);
+                        int rollTwo = Convert.ToInt32(Console.ReadLine());
+                        rollsCount += 1;
+                        frameScore = rollTwo + rollOne;
+                        score += frameScore;
+                        Console.WriteLine("Your score this frame was " + frameScore + " and your total score is " + score);
+                    }
+
+                }
+                else if (frames <= 8 && rollsCount <= 1)
+                {
+
+
+
+
+
+
+                }
+                // Game has passed 10 frames and should be ended
+                else
+                {
+                    Console.WriteLine("Your game is over and your total score is " + score);
+                }
+
+            }
+
+        }
+
+       
     }
 }
